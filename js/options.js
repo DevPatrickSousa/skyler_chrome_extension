f11adea10a8ef574390d16b15aa08d54a27efd9b
patrick <patrick@flyskyler.com>
2024-07-16 13:59:06 -0300

    creating array of tiposBagagem and tiposAssento to can get the correct value

---
+++ js/options.js
@@ -0,0 +1,17 @@
+export const tiposBagagem = [
+  {key: 'sem_bagagem', value: 'Sem bagagem',},
+  {key: '1_mala_23', value: '1 mala de 23kg',},
+  {key: '2_malas_23', value: '2 malas de 23kg'},
+  {key: '3_malas_23', value: '3 malas de 23kg'},
+  {key: '1_mala_32', value: '1 mala de 32kg'},
+  {key: '2_malas_32', value: '2 malas de 32kg'},
+  {key: '3_malas_32', value: '3 malas de 32kg'},
+]
+
+export const tiposAssento = [
+  {key: 'assento_economica', value: 'Econômica',},
+  {key: 'assento_economica_premium', value: 'Econômica premium',},
+  {key: 'assento_executiva', value: 'Executiva'},
+  {key: 'assento_primeira_classe', value: 'Primeira classe'},
+]
+
